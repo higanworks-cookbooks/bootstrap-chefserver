3b6333d07a93360e89239f9881a93fa9fde7a758
sawanoboly <sawanoboriyu@higanworks.com>
2012-09-28 15:13:12 +0900

    modify debconf

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,6 +9,9 @@
 #
 include_recipe "apt"
 
+::Chef::Recipe.send(:include, Opscode::OpenSSL::Password)
+
+
 apt_repository "opscode" do
   uri "http://apt.opscode.com"
   distribution [node.lsb.codename, "-0.10"].join
@@ -24,6 +27,18 @@ execute "apt-get update" do
   command "apt-get update"
   ignore_failure true
   action :run
+end
+
+script "configure dpkgs" do
+  interpreter "bash"
+  code <<-EOH
+  cat <<EOF | debconf-set-selections
+  chef chef/chef_server_url string http://#{node.ipaddress}:4000
+  chef-solr chef-solr/amqp_password password #{secure_password}
+  chef-server-webui chef-server-webui/admin_password password p@ssword
+  EOF
+  EOH
+  not_if File.exist?("/etc/apt/sources.list.d/opscode.update-once.list")
 end
 
 file "/etc/apt/sources.list.d/opscode.update-once.list" do
